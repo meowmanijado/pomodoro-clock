9b3fbaa23252f1ac39ad1634cd5c796ba4415be5
Sharmaine Manijado (DELE) <sharmainemanijado@gmail.com>
2017-11-21 19:04:27 +0800

    Fix App component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Pomodoro from './Pomodoro';
 
+const initialState = {
+  seconds: 1500, 
+  countdown: 0, 
+  pause: false, 
+  timestamp: null, 
+  status1: 'Start', 
+  status2: 'Stop'
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+
+    this.startTimer = this.startTimer.bind(this);
+    this.pauseTimer = this.pauseTimer.bind(this);
+  }
+
+  timer(seconds) {
+    const now = Date.now();
+    const then = now + seconds * 1000;
+
+    clearInterval(this.state.countdown);
+    this.setState({pause: false, status1: 'Resume', status2: 'Pause'});
+
+    this.setState({countdown: setInterval(() => {
+        const secondsLeft = this.setState({seconds: Math.round((then - Date.now()) / 1000)});
+        
+        if (secondsLeft < 0) {
+          clearInterval(this.state.countdown);
+          return;
+        }
+      }, 1000)});
+  }
+
+  startTimer() {
+    this.timer(this.state.seconds);
+  }
+
+  reset() {
+    this.setState(initialState);
+  }
+
+  pauseTimer() {
+    clearTimeout(this.state.countdown);
+    this.setState({pause: true, status2: 'Done', timestamp: Date.now()});
+
+    if (this.state.status2 === 'Done') this.reset();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Pomodoro Clock</h1>
-        </header>
-        <Pomodoro />
+        <Pomodoro time={this.state.seconds} />
+        <button onClick={this.startTimer}>{this.state.status1}</button>
+        <button onClick={this.pauseTimer} disabled={this.state.status1==='Start'}>
+               {this.state.status2}
+        </button>
       </div>
     );
   }
--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -1,58 +1,20 @@
 import React from 'react';
 
 export class Pomodoro extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			seconds: 25 * 60
-		};
 
-		this.startTimer = this.startTimer.bind(this);
-	}
-
-	displayTimer(seconds) {
-		const minutes = Math.floor(this.state.seconds / 60);
-		const remainderSeconds = this.state.seconds % 60;
+	displayTime() {
+		const minutes = Math.floor(this.props.time / 60);
+		const remainderSeconds = this.props.time % 60;
 		const display = `${minutes}: ${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
 		document.title = display;
+
 		return display;
-	}
-
-
-	timer(seconds) {
-		
-		clearInterval(countdown);
-
-		let countdown;
-		const now = Date.now();
-		const then = now + this.state.seconds * 1000;
-		
-
-		this.displayTimer(seconds);
-
-		countdown = setInterval(() => {
-			const secondsLeft = this.setState({seconds: Math.round((then - Date.now()) / 1000)});
-			
-			if (secondsLeft < 0) {
-				clearInterval(countdown);
-				return;
-			}
-			this.displayTimer(secondsLeft);
-		}, 1000);
-
-		
-	}
-
-	startTimer() {
-		const seconds = parseInt(this.displayTimer, 10);
-		this.timer(seconds);
 	}
 
 	render() {
 		return (
 			<div className="display">
-				<h1>{this.displayTimer(this.state.seconds)}</h1>
-				<button onClick={this.startTimer}>Start</button>
+				<h1>{this.displayTime()}</h1>
  			</div>
 		);
 	};
